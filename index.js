9b35ee88edf9b2c4c8299452eb743cb4cfb36dfe
AlejandraNissan <31284199+AlejandraNissan@users.noreply.github.com>
2021-05-18 12:55:41 -0500

    validaciones de form

---
+++ index.js
@@ -0,0 +1,30 @@
+function validarTexto(field){
+    if(field.value.length>2){
+        field.style.background = "white";
+        nombre = true;
+        nombreValor = field.value;
+    }
+    else{
+        field.style.background = "red";
+    }
+}
+
+//Preguntar fecha
+function validarFecha(field){
+    console.log(new Date (field.value));
+    //Checar que la fecha sea anterior a la de hoy
+    if(field.value.length!=null && (new Date(field.value) <= new Date(new Date()))){
+        field.style.background = "white";
+        fNacimiento = true;
+        fNacimientoValor = field.value;
+    }
+    else{
+        field.style.background = "red";
+    }
+}
+
+function submitHandle(form){
+
+    //console.log(document.getElementById());
+    console.log("hola");
+}
