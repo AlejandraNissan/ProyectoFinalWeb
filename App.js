fd08a5c6db0f77aee4dbc52c8e9fcf0f28240276
AlejandraNissan <31284199+AlejandraNissan@users.noreply.github.com>
2021-05-27 08:28:06 -0500

    Leidos ya aparecen

--- App.js
+++ App.js
@@ -3,6 +3,9 @@ var app = express();
 var bodyParser = require('body-parser');
 const {MongoClient} = require('mongodb');
 
+const uri = "mongodb+srv://ProyectoFinalWeb:zgFyV5wbuIJUyoTf@cluster0.ynzjk.mongodb.net/test"
+const client = new MongoClient(uri);
+
 app.use(express.static('Public'));
 
 var jsonParser = bodyParser.json();
@@ -17,9 +20,31 @@ app.post('/APIAgregarLibro', function(req,res) {
     var autor = req.body.Autor;
     var fecha = req.body.Año;
     var genero = req.body.Gen;
-    console.log(nombre + ' | ' + autor + ' | ' + fecha + '|' + genero);
-    libro = {"titulo": nombre, "autor": autor, "fecha": fecha, "genero": genero};
+    var estado = req.body.Estado;
+    console.log(nombre + ' | ' + autor + ' | ' + fecha + '|' + genero + '|' + estado);
+    libro = {"titulo": nombre, "autor": autor, "fecha": fecha, "genero": genero, "estado": estado};
     agregarLibro(libro)
+});
+
+app.get('/APILeido', function(req,res) {
+
+    MongoClient.connect(uri, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("ProyectoFinalWeb");
+        var query = {estado: "Leido"}
+        dbo.collection("Libros").find(query).toArray(function(err, result) {
+          if (err) throw err;
+          console.log(result);
+          res.send(result);
+          db.close();
+        });
+      }); 
+});
+
+app.get('/APILeyendo', function(req,res) {
+});
+
+app.get('/APIPorLeer', function(req,res) {
 });
 
 async function agregarLibro(myobj) {
--- Public/index.js
+++ Public/index.js
@@ -34,3 +34,62 @@ function submitHandle(form){
     document.myForm.submit();
     return false;
 }
+
+async function buscarLeidos(){
+    console.log("Si corre");
+    let book, titulo, autor, estado, img;
+    let imgUrl = "https://picsum.photos/100/200";
+
+    let leidos, leyendo, porLeer;
+
+    leidos = document.getElementById("leidos");
+    leyendo = document.getElementById("leyendo");
+    porLeer = document.getElementById("porLeer");
+
+    leyendo.style.display = "none";
+    porLeer.style.display = "none";
+
+    leidos.style.display = "block";              
+
+    try {
+        const res = await fetch('/APILeido')
+        const data = await res.json()
+        if (data.error !== false) {
+            console.log(data);
+            
+            let listaLeidos = document.getElementById("lista_leidos");
+
+            //Limpiar los libros que estaban anteriormente
+            while (listaLeidos.firstChild) {
+                listaLeidos.removeChild(listaLeidos.firstChild);
+            }          
+
+            for(let i=0; i<data.length; i++){
+                book = document.createElement("li");
+
+                book.setAttribute('id', data[i]._id); //https://www.developerdrive.com/using-custom-attributes-in-html5/
+
+                titulo = document.createElement("h1");
+                titulo.innerHTML = data[i].titulo;
+
+                autor = document.createElement("p");
+                autor.innerHTML = data[i].autor;
+
+                estado = document.createElement("p");
+                estado.innerHTML = data[i].estado;
+
+                img = document.createElement("img");
+                img.src = imgUrl;
+
+                book.appendChild(img);
+                book.appendChild(titulo);
+                book.appendChild(autor);
+                book.appendChild(estado);
+
+                listaLeidos.appendChild(book);
+            }
+        }
+      } catch (error) {
+        console.log(error)
+      }
+}
