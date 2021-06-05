99e12326e71ff3023e3001335e6659dd5d963c39
AlejandraNissan <31284199+AlejandraNissan@users.noreply.github.com>
2021-06-05 13:31:19 -0500

    Muchos avances

--- App.js
+++ App.js
@@ -14,7 +14,6 @@ var urlencodedParser = bodyParser.urlencoded({ extended: true });
 app.use(urlencodedParser)
 
 app.post('/APIAgregarLibro', function(req,res) {
-    console.log("Probando API");
     console.log(req.body.Autor);
     var nombre = req.body.Nombre;
     var autor = req.body.Autor;
@@ -24,6 +23,7 @@ app.post('/APIAgregarLibro', function(req,res) {
     console.log(nombre + ' | ' + autor + ' | ' + fecha + '|' + genero + '|' + estado);
     libro = {"titulo": nombre, "autor": autor, "fecha": fecha, "genero": genero, "estado": estado};
     agregarLibro(libro)
+    res.redirect("/home.html");
 });
 
 app.get('/APILeido', function(req,res) {
@@ -41,28 +41,94 @@ app.get('/APILeido', function(req,res) {
       }); 
 });
 
+app.get('/APIDetalles/:id', function(req,res) {
+    let libro;
+    console.log(req.params.id);
+    var ObjectId = require("mongodb").ObjectID;
+
+    MongoClient.connect(uri, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("ProyectoFinalWeb");
+        var query = {_id: ObjectId(req.params.id)};
+        dbo.collection("Libros").find(query).toArray(function(err, result) {
+            if (err) throw err;
+            res.send(result);
+            db.close();
+          })
+    });
+});
+
+app.post('/APICambiarDatosLibro', function(req,res) {
+    var nombre = req.body.Nombre;
+    var autor = req.body.Autor;
+    var fecha = req.body.Anio;
+    var genero = req.body.Gen;
+    var estado = req.body.Estado;
+    console.log(nombre + ' | ' + autor + ' | ' + fecha + '|' + genero + '|' + estado);
+    libro = {"_id": req.body.Id, "titulo": nombre, "autor": autor, "fecha": fecha, "genero": genero, "estado": estado};
+    cambiarDatosLibro(libro);
+    res.redirect("/home.html");
+});
+
+app.get('/APIEliminar/:id', function(req,res) {
+    var ObjectId = require("mongodb").ObjectID;
+
+    id = req.params.id;
+    console.log(req.params);
+
+    MongoClient.connect(uri, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("ProyectoFinalWeb");
+        var query = {_id: ObjectId(id)}
+        dbo.collection("Libros").deleteOne(query, function(err, obj) {
+            if (err) throw err;
+            console.log("1 document deleted");
+            db.close();
+        });
+        res.sendStatus(200);
+    });
+});
+
 app.get('/APILeyendo', function(req,res) {
+    MongoClient.connect(uri, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("ProyectoFinalWeb");
+        var query = {estado: "Leyendo"}
+        dbo.collection("Libros").find(query).toArray(function(err, result) {
+          if (err) throw err;
+          console.log(result);
+          res.send(result);
+          db.close();
+        });
+      }); 
 });
 
 app.get('/APIPorLeer', function(req,res) {
+    MongoClient.connect(uri, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("ProyectoFinalWeb");
+        var query = {estado: "Por leer"}
+        dbo.collection("Libros").find(query).toArray(function(err, result) {
+          if (err) throw err;
+          console.log(result);
+          res.send(result);
+          db.close();
+        });
+      }); 
 });
 
 async function agregarLibro(myobj) {
-	// we'll add code here soon
     const uri = "mongodb+srv://ProyectoFinalWeb:zgFyV5wbuIJUyoTf@cluster0.ynzjk.mongodb.net/test"
     const client = new MongoClient(uri);
 
     try {
         await client.connect();
-    
-        //await listDatabases(client);
         var dbo = client.db("ProyectoFinalWeb").collection("Libros");
         dbo.insertOne(myobj, function(err, res) {
             if (err) throw err;
-            console.log("1 document inserted");
-            client.close();
-          });
-
+                console.log("1 document inserted");
+                client.close();
+            });
     } catch (e) {
         console.error(e);
     }
@@ -70,5 +136,26 @@ async function agregarLibro(myobj) {
         await client.close();
     }
 } 
+
+async function cambiarDatosLibro(myobj) {
+    var ObjectId = require("mongodb").ObjectID;
+
+    try {
+        await client.connect();
+        
+        var dbo = client.db("ProyectoFinalWeb").collection("Libros");
+        let query = { $set: {titulo: myobj.titulo, autor: myobj.autor, fecha: myobj.fecha, genero: myobj.genero, estado: myobj.estado}};
+        dbo.updateOne({_id: ObjectId(myobj._id)},
+        query,
+        { upsert: true });
+        
+    } catch (e) {
+        console.error(e);
+    }
+    finally {
+        await client.close();
+    }
+} 
+
 
 app.listen(5500);
--- Public/index.js
+++ Public/index.js
@@ -37,7 +37,7 @@ function submitHandle(form){
 
 async function buscarLeidos(){
     console.log("Si corre");
-    let book, titulo, autor, estado, img, btnEditar, btnComment;
+    let id, book, titulo, autor, estado, img, btnEditar;
     let imgUrl = "https://picsum.photos/100/200";
 
     let leidos, leyendo, porLeer;
@@ -83,9 +83,16 @@ async function buscarLeidos(){
 
                 btnEditar = document.createElement("button");
                 btnEditar.innerHTML = "Ver detalles";
+
+                btnEliminar = document.createElement("button");
+                btnEliminar.innerHTML = "-";
                 
                 btnEditar.onclick = function(){
                     mostrarDetalles(data[i]);
+                }
+
+                btnEliminar.onclick = function(){
+                    eliminarLibro(data[i]);
                 }
 
                 console.log(btnEditar);
@@ -95,6 +102,7 @@ async function buscarLeidos(){
                 book.appendChild(autor);
                 book.appendChild(estado);
                 book.appendChild(btnEditar);
+                book.appendChild(btnEliminar);
 
                 listaLeidos.appendChild(book);
             }
@@ -104,8 +112,202 @@ async function buscarLeidos(){
       }
 }
 
-function mostrarDetalles(libro){
-    //document.location.href = 'agregarLibro.html'"
-    console.log("entro aqui")
-    console.log(libro.titulo);
+async function buscarLeyendo(){
+    console.log("Si corre");
+    let id, book, titulo, autor, estado, img, btnEditar;
+    let imgUrl = "https://picsum.photos/100/200";
+
+    let leidos, leyendo, porLeer;
+
+    leidos = document.getElementById("leidos");
+    leyendo = document.getElementById("leyendo");
+    porLeer = document.getElementById("porLeer");
+
+    leidos.style.display = "none";
+    porLeer.style.display = "none";
+
+    leyendo.style.display = "block";              
+
+    try {
+        const res = await fetch('/APILeyendo')
+        const data = await res.json()
+        if (data.error !== false) {
+            console.log(data);
+            
+            let listaLeyendo = document.getElementById("lista_leyendo");
+
+            //Limpiar los libros que estaban anteriormente
+            while (listaLeyendo.firstChild) {
+                listaLeyendo.removeChild(listaLeyendo.firstChild);
+            }          
+
+            for(let i=0; i<data.length; i++){
+                book = document.createElement("li");
+
+                book.setAttribute('id', data[i]._id); //https://www.developerdrive.com/using-custom-attributes-in-html5/
+
+                titulo = document.createElement("h1");
+                titulo.innerHTML = data[i].titulo;
+
+                autor = document.createElement("p");
+                autor.innerHTML = data[i].autor;
+
+                estado = document.createElement("p");
+                estado.innerHTML = data[i].estado;
+
+                img = document.createElement("img");
+                img.src = imgUrl;
+
+                btnEditar = document.createElement("button");
+                btnEditar.innerHTML = "Ver detalles";
+
+                btnEliminar = document.createElement("button");
+                btnEliminar.innerHTML = "-";
+                
+                btnEditar.onclick = function(){
+                    mostrarDetalles(data[i]);
+                }
+
+                btnEliminar.onclick = function(){
+                    eliminarLibro(data[i]);
+                }
+
+                console.log(btnEditar);
+
+                book.appendChild(img);
+                book.appendChild(titulo);
+                book.appendChild(autor);
+                book.appendChild(estado);
+                book.appendChild(btnEditar);
+                book.appendChild(btnEliminar);
+
+                listaLeyendo.appendChild(book);
+            }
+        }
+      } catch (error) {
+        console.log(error)
+      }
 }
+
+async function buscarPorLeer(){
+    console.log("Si corre");
+    let id, book, titulo, autor, estado, img, btnEditar;
+    let imgUrl = "https://picsum.photos/100/200";
+
+    let leidos, leyendo, porLeer;
+
+    leidos = document.getElementById("leidos");
+    leyendo = document.getElementById("leyendo");
+    porLeer = document.getElementById("porLeer");
+
+    leidos.style.display = "none";
+    leyendo.style.display = "none";
+
+    porLeer.style.display = "block";              
+
+    try {
+        const res = await fetch('/APIPorLeer')
+        const data = await res.json()
+        if (data.error !== false) {
+            console.log(data);
+            
+            let listaPorLeer = document.getElementById("lista_por_leer");
+
+            //Limpiar los libros que estaban anteriormente
+            while (listaPorLeer.firstChild) {
+                listaPorLeer.removeChild(listaPorLeer.firstChild);
+            }          
+
+            for(let i=0; i<data.length; i++){
+                book = document.createElement("li");
+
+                book.setAttribute('id', data[i]._id); //https://www.developerdrive.com/using-custom-attributes-in-html5/
+
+                titulo = document.createElement("h1");
+                titulo.innerHTML = data[i].titulo;
+
+                autor = document.createElement("p");
+                autor.innerHTML = data[i].autor;
+
+                estado = document.createElement("p");
+                estado.innerHTML = data[i].estado;
+
+                img = document.createElement("img");
+                img.src = imgUrl;
+
+                btnEditar = document.createElement("button");
+                btnEditar.innerHTML = "Ver detalles";
+
+                btnEliminar = document.createElement("button");
+                btnEliminar.innerHTML = "-";
+                
+                btnEditar.onclick = function(){
+                    mostrarDetalles(data[i]);
+                }
+
+                btnEliminar.onclick = function(){
+                    eliminarLibro(data[i]);
+                }
+
+                console.log(btnEditar);
+
+                book.appendChild(img);
+                book.appendChild(titulo);
+                book.appendChild(autor);
+                book.appendChild(estado);
+                book.appendChild(btnEditar);
+                book.appendChild(btnEliminar);
+
+                listaPorLeer.appendChild(book);
+            }
+        }
+      } catch (error) {
+        console.log(error)
+      }
+}
+
+function mostrarDetalles(libro){
+    let titulo, autor, anio, gen, estado;
+    let myForm;
+
+    console.log("probando");
+    
+    id = libro._id;
+    window.location.href = "detalleslibro.html?id="+id;
+
+}
+
+async function eliminarLibro(libro){
+    console.log(libro._id);
+    try {
+        const res = await fetch('/APIEliminar/'+libro._id)
+        //const data = await res.json()
+        console.log(res);
+        if (res.status == 200) {
+            console.log("recibi 200");
+            window.location.href = "home.html";
+        }
+      } catch (error) {
+        console.log(error)
+      }    
+}
+
+
+      
+    
+    //console.log(libro)
+    
+
+    //myForm.titulo.value = libro.titulo;
+
+    
+    //document.location.href = 'detallesLibro.html';
+    //console.log(this);
+
+
+    //console.log(document);
+    
+    //console.log(document.getElementsByName("myForm"));
+
+    //titulo = document.myForm.Autor;
+    //titulo.value = libro.titulo;
